9f3d386ac4b43f6211cbd66fe32bac48caa776d4
Marion Valls <marion.valls@etu.univ-amu.fr>
2022-11-05 19:54:32 +0100

    feat : adding homepage
    adding homepage background image

--- index.php
+++ index.php
@@ -3,4 +3,11 @@ include './pages/header.php';
 startPage("Accueil",["./assets/style/main"],[]);
 ?>
 
+<div style="position:absolute; top: 15%; left: 5%">
+    <h1>Répondez aux questions pour arriver premier !</h1>
+    <h4>Un serious game pour tester vos connaissances en réseau !</h4>
+    <a href="#connexion" class="black-button">C'EST PARTI</a>
+</div>
+<img src="./assets/image/index_background.webp" width="100%" alt="racing car image" style="opacity: 1">
+<br/>
 
