3a67c52bac121e4bcddcac0b4e0ddadfd3771cc6
Marion Valls <marion.valls@etu.univ-amu.fr>
2022-11-20 17:37:10 +0100

    fix : adding links to header and footer

--- pages/header.php
+++ pages/header.php
@@ -34,35 +34,29 @@ function startPage($A_TITLE, $A_CSS_NAME, $A_JS_SCRIPT){
 
 <div class="header">
     <div class="header-left">
-        <a href="#default"><img src="<?php echo K_IMAGE?>icon_black_small.png" alt="logo" width="90em"></a>
+        <a href="homepage.php"><img src="<?php echo K_IMAGE?>icon_black_small.png" alt="logo" width="90em"></a>
     </div>
     <div class="header-right">
         <div>
-            <div>
-                <a class="active" href="#home">ACCUEIL</a>
-            </div>
-            <div>
-                <a href="#contact">THÈMES</a>
-            </div>
-            <div>
-                <a href="#about">RÈGLES DU JEU</a>
-            </div>
+        <a class="active" href="homepage.php">ACCUEIL</a>
         </div>
         <div>
-            <div>
-            <form method="post" action="#" enctype="text/plain" style="display: flex; flex-direction: row">
-                <input type="text" placeholder="Code multijoueur" required class="input-header">
-                <input type="submit" value="OK" class="submit-header">
-            </form>
-            </div>
-            <div>
-                <a href="#connection" class="hbutton">CONNEXION</a>
-            </div>
+        <a href="#theme">THÈMES</a>
+        </div>
+        <div>
+        <a href="rules.php">RÈGLES DU JEU</a>
+        </div>
+        <div>
+        <form method="post" action="#" enctype="text/plain" style="display: flex; flex-direction: row">
+            <input type="text" placeholder="Code multijoueur" required class="input-header">
+            <input type="submit" value="OK" class="submit-header">
+        </form>
+        </div>
+        <div>
+        <a href="connection.php" class="hbutton">CONNEXION</a>
         </div>
     </div>
 </div>
 
-
-<!-- TODO : add a button go back on top of the page-->
 <?php
 }?>
