7e0d216575ddb398d8c0f3459e59167ebcd70a1e
Marion Valls <marion.valls@etu.univ-amu.fr>
2022-12-05 09:21:06 +0100

    fix (themes) : images and css

--- pages/themes.php
+++ pages/themes.php
@@ -8,38 +8,34 @@ startPage("Themes",["../assets/style/main", "../assets/style/themes"],["../asset
     <button id="adressage" class="button_theme" onclick="show_theme('adressage')"><b>Adressage</b></button>
 </div>
 <div class="all_theme">
-    <h1 id="waiting">Merci de sélectionné au minimum un theme</h1>
+    <h1 id="waiting">Merci de sélectionner au minimum un thème</h1>
     <div class="adressage">
-        <img class="tour" src="../assets/image/circuit1.jpg" alt="zircui">
+        <img class="tour" src="../assets/image/circuit1.jpg" alt="circuit">
         <div class="progress_bar">
             <div class="progress" style="width:25%" >25%</div>
         </div>
         <p>Adressage: adresse IP, adresse</p>
-        <img class="difficulty" src="../assets/image/compteur_easy.png" alt="difficulte">
     </div>
     <div class="adressage">
-        <img class="tour" src="../assets/image/circuit1.jpg" alt="zircui">
+        <img class="tour" src="../assets/image/circuit1.jpg" alt="circuit">
         <div class="progress_bar">
             <div class="progress" style="width:50%">50%</div>
         </div>
         <p>Adressage: adresse IP, adresse</p>
-        <img class="difficulty" src="../assets/image/compteur_easy.png" alt="difficulte">
     </div>
     <div class="reseau">
-        <img class="tour" src="../assets/image/circuit1.jpg" alt="zircui">
+        <img class="tour" src="../assets/image/circuit1.jpg" alt="circuit">
         <div class="progress_bar">
             <div class="progress" style="width:75%">75%</div>
         </div>
         <p>Adressage: adresse IP, adresse</p>
-        <img class="difficulty" src="../assets/image/compteur_easy.png" alt="difficulte">
     </div>
     <div class="reseau">
-        <img class="tour" src="../assets/image/circuit1.jpg" alt="zircui">
+        <img class="tour" src="../assets/image/circuit1.jpg" alt="circuit">
         <div class="progress_bar">
             <div class="progress" style="width:100%">100% </div>
         </div>
         <p>Adressage: adresse IP, adresse</p>
-        <img class="difficulty" src="../assets/image/compteur_easy.png" alt="difficulte">
     </div>
 </div>
 
