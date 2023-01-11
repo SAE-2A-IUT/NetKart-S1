1a7d17c966ee4f37ea32f207952d6907045062bc
Alessioplt <alessio.pulit@gmail.com>
2023-01-11 14:17:41 +0100

    feat: UI for deleting user account w/ confirmation

--- pages/user.php
+++ pages/user.php
@@ -8,7 +8,6 @@ startPage("Utilisateur",[K_STYLE."main",K_STYLE."user"],[]);
             <h1>User1207</h1>
             <div class="new_password">
                 <span>Changer son mot de passe :</span>
-                <!-- ajouter un message d'alerte en php -->
                 <form class="new_psw_form" method="post" action="#">
                     <label>Nouveau mot de passe</label>
                     <input name="new_password" type="password">
@@ -20,6 +19,32 @@ startPage("Utilisateur",[K_STYLE."main",K_STYLE."user"],[]);
         </div>
         <div class="right">
             <img src="<?php echo K_IMAGE; ?>default_avatar.jpg">
+            <button id="delete_account" class="delete">Supprimer compte</button>
+            <div id="horizontal" class="horizontal">
+                <form>
+                    <input type="submit" value="Continuer" class="send_from" id="button_click">
+                </form>
+                <button id="cancel_delete_account">annuler</button>
+            </div>
+
+            <p id="alert">etes vous sûr? <br>les circuits que vous avez créer seront egalement supprimé les circuits que vous avez créer seront egalement supprimé</p>
+            <script>
+                document.getElementById("horizontal").style.display ="none";
+                document.getElementById("alert").style.display ="none";
+
+                function delete_account() {
+                    document.getElementById("horizontal").style.display = "flex";
+                    document.getElementById("alert").style.display = "flex";
+                }
+
+                function cancel_delete_account() {
+                    document.getElementById("horizontal").style.display = "none";
+                    document.getElementById("alert").style.display = "none";
+                }
+
+                document.getElementById('delete_account').addEventListener('click', delete_account);
+                document.getElementById('cancel_delete_account').addEventListener('click', cancel_delete_account);
+            </script>
         </div>
     </div>
 
