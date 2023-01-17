f48932fde825be26084d8c2d9d0bced54355df07
Thibaut LAHAIE <thibaut.lahaie@etu.univ-amu.fr>
2023-01-17 14:38:26 +0100

    wip(BD): select every informations of the circuit #1

--- pages/game-solo.php
+++ pages/game-solo.php
@@ -1,59 +1,50 @@
 <?php
 require './header.php';
-startPage("Jeu-solo", ["../assets/style/main", "../assets/style/game-solo"], ["../assets/script/position"]);
+startPage("Jeu-solo",["../assets/style/main", "../assets/style/game-solo"],[]);
+require("./database/database.php");
+
+$l_db = new database();
+$l_db->connection();
+
+$id_circuit = 2;
+$name_circuit = $l_db->get_circuit_information($id_circuit)[0]['nom_circuit'];
+$id_circuitimage = $l_db->get_circuit_information($id_circuit)[0]['id_circuitimage'];
+
 ?>
+
+
 <div class="body-page">
     <div id="game">
         <div id="left-game">
             <div id="circuit-info">
-                <h1 id="circuit-name">Nom du circuit</h1>
+                <h1 id="circuit-name"><?php echo $name_circuit?></h1>
                 <h5 id="question-number">Numéro de la question :</h5>
-                <img alt="image1-origin" class="question-image-origin"
-                     src="https://i.ytimg.com/vi/PQX-TQDWrlw/maxresdefault.jpg">
+                <img alt="image1-origin" class="question-image-origin" src="https://i.ytimg.com/vi/PQX-TQDWrlw/maxresdefault.jpg">
                 <img alt="image1" class="question-image" src="https://i.ytimg.com/vi/PQX-TQDWrlw/maxresdefault.jpg">
-                <img alt="image2-origin" class="question-image-origin"
-                     src="https://i.ytimg.com/vi/NBMuB-iVQ2U/maxresdefault.jpg">
+                <img alt="image2-origin" class="question-image-origin" src="https://i.ytimg.com/vi/NBMuB-iVQ2U/maxresdefault.jpg">
                 <img alt="image2" class="question-image" src="https://i.ytimg.com/vi/NBMuB-iVQ2U/maxresdefault.jpg">
-                <img alt="image3-origin" class="question-image-origin"
-                     src="https://i.ytimg.com/vi/G8yMRnM2Ymw/maxresdefault.jpg">
+                <img alt="image3-origin" class="question-image-origin" src="https://i.ytimg.com/vi/G8yMRnM2Ymw/maxresdefault.jpg">
                 <img alt="image3" class="question-image" src="https://i.ytimg.com/vi/G8yMRnM2Ymw/maxresdefault.jpg">
-                <p id="question-statement">Consigne de la question c'est pas la carte igne de la questioigne de la
-                    questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la
-                    questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la
-                    questioigne de la questioigne de la questioigne de la questio</p>
+                <!-- image si il y en a une quoi -->
+                <p id="question-statement">Consigne de la question c'est pas la carte igne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questioigne de la questio</p>
 
             </div>
-            <div id="terminal">
-                <div id="terminal-output"></div>
-                <label for="terminal-input"></label>
-                <span id="terminal-input-text">NetKart:~$<input id="terminal-input" autocomplete="off"
-                                                                placeholder="tapez votre commande ici"></span>
+            <div>
+                <p id="terminal">Ici il y aura le terminal mais bon pour l'instant on en a pas donc voila quoien a pas donc voila quoi...en a pas donc voila quoi...en a pas donc voila quoi...en a pas donc voila quoi..as donc voila quoi...en a pas donc voila quoi...en a pas donc voila quoi...eas donc voila quoi...en a pas donc voila quoi...en a pas donc voila quoi...eas donc voila quoi...en a pas donc voila quoi...en a pas donc voila quoi...eas donc voila quoi...en a pas donc voila quoi...en a pas donc voila quoi...eas donc voila quoi...en a pas donc voila quoi...en a pas donc voila quoi...e.en a pas donc voila quoi...en a pas donc voila quoi.......</p>
             </div>
-            <script>
-            </script>
         </div>
 
-        <div id="right-game" style="background-image: url('../assets/image/circuit1.jpg');">
-            <img src="../assets/image/gentil.webp" alt="player-kart" id="player_kart">
-            <img src="../assets/image/mechant.webp" alt="enemy-kart" id="enemy_kart">
-            <img src="../assets/image/flag-start.webp" alt="flag" id="flag">
-        </div>
-    </div>
-    <div id="modal" style="display: none;">
-        <div id="modal-content">
-            <div id="modal-header">
-                <span id="modal-close">&times;</span>
-            </div>
-            <div id="modal-body">null</div>
-            <div id="modal-footer">
-                <button id="modal-return" type="button">Retour</button>
-                <button id="modal-restart" type="button">Recommencer</button>
-            </div>
+        <div id="right-game">
+            <img id="map" alt="circuit" src="../assets/image/circuit1.jpg">
         </div>
     </div>
 </div>
 
+
+
+
 <?php
 require './footer.php';
 endPage();
+$l_db->close();
 ?>
