d5054c30a2180c9c9dabee4dc859f57c0b2a07f4
Marion Valls <marion.valls@etu.univ-amu.fr>
2023-01-17 15:32:54 +0100

    fix (footer) : adding links

--- pages/footer.php
+++ pages/footer.php
@@ -12,19 +12,19 @@ function endPage(){
         <div class="footer-one"><img src="<?php echo K_IMAGE ?>icon_black.png" alt="NetKart Icon"></div>
         <div class="footer-two">
             PAGES<br/>
-            <a href="#homepage">Accueil</a>
-            <br/><a href="#themes">Thèmes</a>
-            <br/><a href="#rules">Règles du jeu</a>
+            <a href="homepage.php">Accueil</a>
+            <br/><a href="themes.php">Thèmes</a>
+            <br/><a href="rules.php">Règles du jeu</a>
         </div>
         <div class="footer-three">
             DOCUMENTS<br/>
-            <a href="#rules">Règles du jeu</a>
-            <br/><a href="#legal">Mention Légales</a>
+            <a href="rules.php">Règles du jeu</a>
+            <br/><a href="terms-of-use.php">Mention Légales</a>
         </div>
         <div class="footer-three">
             NETKART<br/>
-            <a href="#about">A propos</a>
-            <br/><a href="#contact">Nous contacter</a>
+            <a href="about.php">A propos</a>
+            <br/><a href="contact.php">Nous contacter</a>
         </div>
     </footer>
 
