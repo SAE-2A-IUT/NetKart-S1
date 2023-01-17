14d5cff30462b6143ce90a5f4b4fccc143c4fa05
Alessioplt <alessio.pulit@gmail.com>
2023-01-17 17:32:01 +0100

    (wip) edit theme

--- pages/edit_circuit.php
+++ pages/edit_circuit.php
@@ -8,8 +8,7 @@
  */
 require ("./database/database.php");
 include './header.php';
-startPage("Themes",["../assets/style/main", "../assets/style/themes"],["../assets/script/theme"]);
-
+startPage("Edit",["../assets/style/main", "../assets/style/edit_theme"],["../assets/script/theme"]);
 /*
  * Check if password and confirmation are set
  */
@@ -26,23 +25,56 @@ if(True){ # isset($_SESSION id joueur
     $l_id_circuit_user = $l_db->get_circuit_created_by_user($l_user_id);
 
     // Get the information of circuits created by user
+    $l_count_loop = 0;
+    ?>
+    <div class="all_theme">
+    <?php
     foreach ($l_id_circuit_user as $item){
         ?>
-        <form method="post" action="edit_circuit_post.php">
+
             <?php
         $l_circuit_informations = $l_db->get_circuit_information($item["id_circuit"]);
-        foreach ($l_circuit_informations as $item){
-            echo "Circuit id : ".$item["id_circuit"].PHP_EOL.
-                "Nom circuit".$item["nom_circuit"].PHP_EOL.
-                "Nombre points".$item["points"].PHP_EOL.
-                $l_img = $l_db->get_image_circuit($item["id_circuitimage"])[0]["image"];
-                "Circuit image".$l_img.PHP_EOL;
-        }?>
-            <input type="hidden" value="<?php echo $item["id_circuit"]; ?>" name="id_circuit_to_delete">
-            <input type="submit" value="Supprimer">
-        </form>
+        foreach ($l_circuit_informations as $item):?>
+            <?php $l_count_loop = $l_count_loop + 1; ?>
+        <div class="modify_delete">
+            <div class="theme">
+                <img class="theme_image" alt="circuit" src=<?=K_IMAGE . $l_db->get_image_circuit($item["id_circuitimage"])[0]["image"].PHP_EOL;?>>
+                <h3><?= $item["nom_circuit"].PHP_EOL;?></h3>
+                <p><?= $item["points"].PHP_EOL;?></p>
+                <div class="form_div">
+                    <form class="modify" method="post" action="new-circuit_post.php">
+                        <input type="hidden" value="<?php echo $item["id_circuit"]; ?>" name="id_circuit_to_modify">
+                        <input type="submit" value="Modifier">
+                    </form>
+                    <form class="delete" method="post" action="">
+                        <input type="hidden" value="<?php echo $item["id_circuit"]; ?>" name="id_circuit_to_delete">
+                        <input type="submit" value="Supprimer">
+                    </form>
+                    <input type="checkbox" class="test">
+                    <div>
+                        <span>
+                            test
+                        </span>
+                    </div>
+                    <form  method="post" action="">
+                            <input type="hidden" value="<?php echo $item["id_circuit"]; ?>" name="id_circuit_to_delete">
+                            <input type="submit" value="test">
+                        </form>
+
+                </div>
+            </div>
+
+        </div>
+        <?php endforeach; ?>
         <?php
     }
+    ?></div><?php
+    if ($l_count_loop == 0) {
+        ?>
+        <img class="no_circuit" alt="circuit" src=<?=K_IMAGE."no_circuit.jpg";?>>
+        <?php
+    }
+
 
     //TODO : afficher les données sous forme d'éléments comme dans la page thèmes
 
