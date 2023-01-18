a7fe03a01517a353e7eb79f343cdf66d87c30b2f
Alessioplt <alessio.pulit@gmail.com>
2023-01-18 16:42:52 +0100

    feat: edit circuit css + added new circuit button + correction of mademoiselle

--- pages/edit_circuit.php
+++ pages/edit_circuit.php
@@ -9,6 +9,7 @@
 require ("./database/database.php");
 include './header.php';
 startPage("Edit",["../assets/style/main", "../assets/style/edit_theme"],["../assets/script/theme"]);
+
 /*
  * Check if password and confirmation are set
  */
@@ -27,39 +28,48 @@ if(True){ # isset($_SESSION id joueur
     // Get the information of circuits created by user
     $l_count_loop = 0;
     ?>
+    <div class="create_ciruit" id="UwU">
+    <form class="create" method="post" action="new-circuit.php">
+        <input type="submit" value="Créer un circuit">
+    </form>
+    </div>
     <div class="all_theme">
+
     <?php
-    foreach ($l_id_circuit_user as $item){
+    foreach ($l_id_circuit_user as $items){
         ?>
 
             <?php
-        $l_circuit_informations = $l_db->get_circuit_information($item["id_circuit"]);
+        $l_circuit_informations = $l_db->get_circuit_information($items["id_circuit"]);
         foreach ($l_circuit_informations as $item):?>
             <?php $l_count_loop = $l_count_loop + 1; ?>
+        <script>
+            function Confirm(count) {
+                document.getElementById("first_delete" + count).style.display = "none";
+                document.getElementById("delete" + count).style.display = "block";
+            }
+
+            function redirect(link) {
+                window.open(link);
+            }
+        </script>
         <div class="modify_delete">
             <div class="theme">
                 <img class="theme_image" alt="circuit" src=<?=K_IMAGE . $l_db->get_image_circuit($item["id_circuitimage"])[0]["image"].PHP_EOL;?>>
                 <h3><?= $item["nom_circuit"].PHP_EOL;?></h3>
-                <p><?= $item["points"].PHP_EOL;?></p>
+                <p class="points">Points: <?= $item["points"].PHP_EOL;?></p>
                 <div class="form_div">
                     <form class="modify" method="post" action="new-circuit_post.php">
                         <input type="hidden" value="<?php echo $item["id_circuit"]; ?>" name="id_circuit_to_modify">
                         <input type="submit" value="Modifier">
                     </form>
-                    <form class="delete" method="post" action="">
-                        <input type="hidden" value="<?php echo $item["id_circuit"]; ?>" name="id_circuit_to_delete">
+                    <form class="first_delete" id="first_delete<?= $l_count_loop;?>" method="post" action="javascript:Confirm(<?= $l_count_loop;?>)">
                         <input type="submit" value="Supprimer">
                     </form>
-                    <input type="checkbox" class="test">
-                    <div>
-                        <span>
-                            test
-                        </span>
-                    </div>
-                    <form  method="post" action="">
+                    <form class="delete" id="delete<?= $l_count_loop;?>" method="post" action="edit_circuit_post.php">
                             <input type="hidden" value="<?php echo $item["id_circuit"]; ?>" name="id_circuit_to_delete">
-                            <input type="submit" value="test">
-                        </form>
+                            <input type="submit" value="Confirmer">
+                    </form>
 
                 </div>
             </div>
@@ -71,7 +81,13 @@ if(True){ # isset($_SESSION id joueur
     ?></div><?php
     if ($l_count_loop == 0) {
         ?>
-        <img class="no_circuit" alt="circuit" src=<?=K_IMAGE."no_circuit.jpg";?>>
+        <script>document.getElementById("UwU").style.display = "none";</script>
+        <div class="no_circuit">
+            <h1>Aucun circuit n'a encore été crée</h1>
+            <form class="create2" method="post" action="new-circuit.php">
+                <input type="submit" value="Créer un circuit">
+            </form>
+        </div>
         <?php
     }
 
